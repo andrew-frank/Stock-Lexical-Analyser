1376be35b1ee02bacf97aa639be8368fb6f29248
frandrzej <andrewfrankowski@gmail.com>
2015-03-14 13:59:41 +0100

    Small refactor in parser. Finds NIP and Full issuer name.

--- src/html_parser.pl
+++ src/html_parser.pl
@@ -22,8 +22,8 @@
 :- use_module(library(xpath)).
 
 
-% example:
-% parse_sample(DOM,_).
+% example: parse_sample(DOM,_).
+% for now will print out Full issuer name and it's NIP
 
 %parse_sample(-DOM, ?Element)
 parse_sample(DOM, Element) :-
@@ -41,26 +41,52 @@ parse_report(Html, DOM, Element) :-
 	write('Parsing '), writeln(Html),
 	load_html(Html, DOM, []),
 	writeln('Html loaded and converted to list. Searching for fields'),
+	find_message_table(DOM, MessageTableBody),
+	%data_at_index_path(MessageTableBody, Data),
+	%writeln('Found message data cell: '),
+	%writeln(Data).
+	find_company_data_table(DOM, CompanyDataTable),
+	full_issuer_name(CompanyDataTable, FullName),
+	write('Full issuer name: '),
+	writeln(FullName),
+	nip(CompanyDataTable, Nip),
+	write('NIP: '),
+	writeln(Nip).
+
+% find_message_table(-DOM, +Table)
+% finds table body with the stock message
+find_message_table(DOM, TableDataBody) :-
 	xpath(DOM, //div(table), Div),
 	xpath(Div, //table, Table1),
 	xpath(Table1, //tbody, TBody1),
 	xpath(TBody1, //tr, Tr1),
 	xpath(Tr1, //td, Td1),
 	xpath(Td1, //table, Table2),
-	xpath(Table2, //tbody(tr), TBody2),
-        %writeln('Table 2'), writeln(Tbody2),
-	xpath(TBody2, //tr, Tr2),
-	xpath(Tr2, //td(2), Td2),
-	writeln(' *************** ELEMENT ***************'), writeln(Td2).
-	%writeln('Element'), writeln(Element).
-	%Element = element(Element, [], E),
-	%write('Element: '), writeln(Element).
+	xpath(Table2, //tbody(tr(@class=nTekst)), TableDataBody).
 
 
+% (-DOM, +CompanyDataTable)
+% finds table body with company data
+find_company_data_table(DOM, CompanyDataTable) :-
+	xpath(DOM, //div(table), Div),
+	xpath(Div, //table(@class=nDokument), CompanyDataTable).
 
+% (-, +)
+full_issuer_name(CompanyDataTable, Name) :-
+	xpath(CompanyDataTable, //tr(4), Tr),
+	xpath(Tr, //td(2), Kupa),
+	Kupa = element(td, _, Name).
 
+% (-, +)
+nip(CompanyDataTable, NIP) :-
+	xpath(CompanyDataTable, //tr(15), Tr),
+	xpath(Tr, //td(2), Kupa),
+	Kupa = element(td, _, NIP).
 
-
-
-
-
+% temp - passing arguments to //tr(Row) and //td(Col) does not work :(
+% this language is stupid as shit and we'll have to type manually cell
+% index paths
+data_at_index_path(TBody, Data) :-
+	xpath(TBody, //tr(5), Tr2),
+	xpath(Tr2, //td(2), Kupa),
+	Data = element(td, [],Kupa).
